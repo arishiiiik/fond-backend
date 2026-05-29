7c803e9c6e6ed76f6a6941d8da175cc57f08e121
arishiiiik <arishiiiik@example.com>
2026-05-29 04:20:56 +0300

    Temporary create superuser

--- api/models.py
+++ api/models.py
@@ -233,3 +233,8 @@ class VolunteerRequest(models.Model):
 
     def __str__(self):
         return f"{self.name} - {self.city}"
+    
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(is_superuser=True).exists():
+    User.objects.create_superuser('admin', 'admin@example.com', 'admin123')
