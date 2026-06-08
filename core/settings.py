32767811bb8ded562546e9c12cf7610d78623c10
arishiiiik <arishiiiik@example.com>
2026-06-09 01:28:53 +0300

    настройка изображений

--- core/settings.py
+++ core/settings.py
@@ -20,11 +20,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'cloudinary_storage',
-    'cloudinary',          
     'rest_framework',
     'rest_framework_simplejwt',
     'corsheaders',
+    'cloudinary_storage',
+    'cloudinary',
     'api',
 ]
 
